6077293b3ec82e70cbf93a03f2d35722f23c31a0
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2023-01-03 16:55:35 +0900

    Introduce OpenAPI Schema

--- fridgelog/fridgelog/urls.py
+++ fridgelog/fridgelog/urls.py
@@ -1,24 +1,14 @@
-"""fridgelog URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
+from rest_framework.schemas import get_schema_view
 
 from foods import views
 
 urlpatterns = [
     path("foods/", views.FoodList.as_view()),
     path("foods/<int:pk>/", views.FoodDetail.as_view()),
+    path('openapi', get_schema_view(
+        title="FridgeLog",
+        description="API for fridgelog"
+    ), name='openapi-schema'),
     # path('admin/', admin.site.urls),
 ]
